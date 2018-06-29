c0aa948d8c6315943ea43ffaec107af500272a03
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 12:37:34 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,15 @@
 module Memorable
 
-  def reset_all
-    all.clear
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
+
+    def count
+      all.count
+    end
   end
 
-  def count
-    all.count
-  end
+  module InstanceMethods
 
 end
